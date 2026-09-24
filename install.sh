f2af07721f6f5c17862516045fe8fcb2c261c9db
liming <827182486@qq.com>
2026-09-24 23:31:50 +0800

    增加Telegram推送节点通知

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # 等待1秒, 避免curl下载脚本的打印与脚本本身的显示冲突, 吃掉了提示用户按回车继续的信息
 sleep 1
+export LANG=en_US.UTF-8
 
 echo -e "                     _ ___                   \n ___ ___ __ __ ___ _| |  _|___ __ __   _ ___ \n|-_ |_  |  |  |-_ | _ |   |- _|  |  |_| |_  |\n|___|___|  _  |___|___|_|_|___|  _  |___|___|\n        |_____|               |_____|        "
 red='\e[91m'
@@ -8,18 +9,22 @@ yellow='\e[93m'
 magenta='\e[95m'
 cyan='\e[96m'
 none='\e[0m'
-
+red() { echo -e "\033[31m\033[01m$1\033[0m"; }
+green() { echo -e "\033[32m\033[01m$1\033[0m"; }
+yellow() { echo -e "\033[33m\033[01m$1\033[0m"; }
+blue() { echo -e "\033[36m\033[01m$1\033[0m"; }
+white() { echo -e "\033[37m\033[01m$1\033[0m"; }
 error() {
-    echo -e "\n${red} 输入错误! ${none}\n"
+	echo -e "\n${red} 输入错误! ${none}\n"
 }
 
 warn() {
-    echo -e "\n$yellow $1 $none\n"
+	echo -e "\n$yellow $1 $none\n"
 }
 
 pause() {
-    read -rsp "$(echo -e "按 ${green} Enter 回车键 ${none} 继续....或按 ${red} Ctrl + C ${none} 取消.")" -d $'\n'
-    echo
+	read -rsp "$(echo -e "按 ${green} Enter 回车键 ${none} 继续....或按 ${red} Ctrl + C ${none} 取消.")" -d $'\n'
+	echo
 }
 
 # 说明
@@ -34,19 +39,19 @@ echo "----------------------------------------------------------------"
 apt-get -y install curl wget -qq
 
 # 本机 IP
-InFaces=($(ls /sys/class/net/ | grep -E '^(eth|ens|eno|esp|enp|venet|vif)'))  #找所有的网口
+InFaces=($(ls /sys/class/net/ | grep -E '^(eth|ens|eno|esp|enp|venet|vif)')) #找所有的网口
 
-for i in "${InFaces[@]}"; do  # 从网口循环获取IP
-    # 增加超时时间, 以免在某些网络环境下请求IPv6等待太久
-    Public_IPv4=$(curl -4s --interface "$i" -m 2 https://www.cloudflare.com/cdn-cgi/trace | grep -oP "ip=\K.*$")
-    Public_IPv6=$(curl -6s --interface "$i" -m 2 https://www.cloudflare.com/cdn-cgi/trace | grep -oP "ip=\K.*$")
+for i in "${InFaces[@]}"; do # 从网口循环获取IP
+	# 增加超时时间, 以免在某些网络环境下请求IPv6等待太久
+	Public_IPv4=$(curl -4s --interface "$i" -m 2 https://www.cloudflare.com/cdn-cgi/trace | grep -oP "ip=\K.*$")
+	Public_IPv6=$(curl -6s --interface "$i" -m 2 https://www.cloudflare.com/cdn-cgi/trace | grep -oP "ip=\K.*$")
 
-    if [[ -n "$Public_IPv4" ]]; then  # 检查是否获取到IP地址
-        IPv4="$Public_IPv4"
-    fi
-    if [[ -n "$Public_IPv6" ]]; then  # 检查是否获取到IP地址            
-        IPv6="$Public_IPv6"
-    fi
+	if [[ -n "$Public_IPv4" ]]; then # 检查是否获取到IP地址
+		IPv4="$Public_IPv4"
+	fi
+	if [[ -n "$Public_IPv6" ]]; then # 检查是否获取到IP地址
+		IPv6="$Public_IPv6"
+	fi
 done
 
 # 通过IP, host, 时区, 生成UUID. 重装脚本不改变, 不改变节点信息, 方便个人使用
@@ -63,54 +68,98 @@ default_port=2096
 
 # 执行脚本带参数
 if [ $# -ge 1 ]; then
-    # 第1个参数是搭在ipv4还是ipv6上
-    case ${1} in
-    4)
-        netstack=4
-        ip=${IPv4}
-        ;;
-    6)
-        netstack=6
-        ip=${IPv6}
-        ;;
-    *) # initial
-        if [[ -n "$IPv4" ]]; then  # 检查是否获取到IP地址
-            netstack=4
-            ip=${IPv4}
-        elif [[ -n "$IPv6" ]]; then  # 检查是否获取到IP地址            
-            netstack=6
-            ip=${IPv6}
-        else
-            warn "没有获取到公共IP"
-        fi
-        ;;
-    esac
+	# 第1个参数是搭在ipv4还是ipv6上
+	case ${1} in
+	4)
+		netstack=4
+		ip=${IPv4}
+		;;
+	6)
+		netstack=6
+		ip=${IPv6}
+		;;
+	*)                         # initial
+		if [[ -n "$IPv4" ]]; then # 检查是否获取到IP地址
+			netstack=4
+			ip=${IPv4}
+		elif [[ -n "$IPv6" ]]; then # 检查是否获取到IP地址
+			netstack=6
+			ip=${IPv6}
+		else
+			warn "没有获取到公共IP"
+		fi
+		;;
+	esac
 
-    # 第2个参数是port
-    port=${2}
-    if [[ -z $port ]]; then
-      port=${default_port}
-    fi
+	# 第2个参数是port
+	port=${2}
+	if [[ -z $port ]]; then
+		port=${default_port}
+	fi
 
-    # 第3个参数是域名
-    domain=${3}
-    if [[ -z $domain ]]; then
-      domain="learn.microsoft.com"
-    fi
+	# 第3个参数是域名
+	domain=${3}
+	if [[ -z $domain ]]; then
+		domain="learn.microsoft.com"
+	fi
 
-    # 第4个参数是密码
-    pwd=${4}
-    if [[ -z $pwd ]]; then
-        pwd=${default_uuid}
-    fi
+	# 第4个参数是密码
+	pwd=${4}
+	if [[ -z $pwd ]]; then
+		pwd=${default_uuid}
+	fi
 
-    echo -e "$yellow netstack = ${cyan}${netstack}${none}"
-    echo -e "$yellow 本机IP = ${cyan}${ip}${none}"
-    echo -e "${yellow} 端口 (Port) = ${cyan}${port}${none}"
-    echo -e "${yellow} 密码 (Password) = ${cyan}${pwd}${none}"
-    echo -e "${yellow} 自签证书所用域名 (Certificate Domain) = ${cyan}${domain}${none}"
-    echo "----------------------------------------------------------------"
+	echo -e "$yellow netstack = ${cyan}${netstack}${none}"
+	echo -e "$yellow 本机IP = ${cyan}${ip}${none}"
+	echo -e "${yellow} 端口 (Port) = ${cyan}${port}${none}"
+	echo -e "${yellow} 密码 (Password) = ${cyan}${pwd}${none}"
+	echo -e "${yellow} 自签证书所用域名 (Certificate Domain) = ${cyan}${domain}${none}"
+	echo "----------------------------------------------------------------"
 fi
+
+tgsbshow() {
+	echo
+	yellow "1：重置/设置Telegram机器人的Token、用户ID"
+	readp "输入Telegram机器人Token: " token
+	telegram_token=$token
+	readp "输入Telegram机器人用户ID: " userid
+	telegram_id=$userid
+	echo '#!/bin/bash
+export LANG=en_US.UTF-8
+
+m1=$(cat ~/_hy2_url_ 2>/dev/null)
+message_text_m1=$(echo "$m1")
+MODE=HTML
+URL="https://api.telegram.org/bottelegram_token/sendMessage"
+res=$(timeout 20s curl -s -X POST $URL -d chat_id=telegram_id  -d parse_mode=${MODE} --data-urlencode "text=🚀【 Vmess-ws-tls 分享链接 】：支持v2rayng、nekobox "$'"'"'\n\n'"'"'"${message_text_m1}")
+
+
+if [ $? == 124 ];then
+echo TG_api请求超时,请检查网络是否重启完成并是否能够访问TG
+fi
+resSuccess=$(echo "$res" | jq -r ".ok")
+if [[ $resSuccess = "true" ]]; then
+echo "TG推送成功";
+else
+echo "TG推送失败，请检查TG机器人Token和ID";
+fi
+' >/etc/s-box/sbtg.sh
+	sed -i "s/telegram_token/$telegram_token/g" /etc/s-box/sbtg.sh
+	sed -i "s/telegram_id/$telegram_id/g" /etc/s-box/sbtg.sh
+	green "设置完成！请确保TG机器人已处于激活状态！"
+	tgnotice
+
+}
+
+tgnotice() {
+	if [[ -f /etc/s-box/sbtg.sh ]]; then
+		green "请稍等5秒，TG机器人准备推送……"
+		bash /etc/s-box/sbtg.sh
+	else
+		yellow "未设置TG通知功能"
+	fi
+	exit
+}
 
 pause
 
@@ -134,72 +183,72 @@ echo "----------------------------------------------------------------"
 
 # 网络栈
 if [[ -z $netstack ]]; then
-  echo
-  echo -e "如果你的小鸡是${magenta}双栈(同时有IPv4和IPv6的IP)${none}，请选择你把Hy2搭在哪个'网口'上"
-  echo "如果你不懂这段话是什么意思, 请直接回车"
-  read -p "$(echo -e "Input ${cyan}4${none} for IPv4, ${cyan}6${none} for IPv6:") " netstack
+	echo
+	echo -e "如果你的小鸡是${magenta}双栈(同时有IPv4和IPv6的IP)${none}，请选择你把Hy2搭在哪个'网口'上"
+	echo "如果你不懂这段话是什么意思, 请直接回车"
+	read -p "$(echo -e "Input ${cyan}4${none} for IPv4, ${cyan}6${none} for IPv6:") " netstack
 
-  if [[ $netstack == "4" ]]; then
-    ip=${IPv4}
-  elif [[ $netstack == "6" ]]; then
-    ip=${IPv6}
-  else
-    if [[ -n "$IPv4" ]]; then  # 检查是否获取到IP地址
-        netstack=4
-        ip=${IPv4}
-    elif [[ -n "$IPv6" ]]; then  # 检查是否获取到IP地址            
-        netstack=6
-        ip=${IPv6}
-    else
-        warn "没有获取到公共IP"
-    fi    
-  fi
+	if [[ $netstack == "4" ]]; then
+		ip=${IPv4}
+	elif [[ $netstack == "6" ]]; then
+		ip=${IPv6}
+	else
+		if [[ -n "$IPv4" ]]; then # 检查是否获取到IP地址
+			netstack=4
+			ip=${IPv4}
+		elif [[ -n "$IPv6" ]]; then # 检查是否获取到IP地址
+			netstack=6
+			ip=${IPv6}
+		else
+			warn "没有获取到公共IP"
+		fi
+	fi
 fi
 
 # 端口
 if [[ -z $port ]]; then
-  while :; do
-    read -p "$(echo -e "请输入端口 [${magenta}1-65535${none}] Input port (默认Default ${cyan}${default_port}$none):")" port
-    [ -z "$port" ] && port=$default_port
-    case $port in
-    [1-9] | [1-9][0-9] | [1-9][0-9][0-9] | [1-9][0-9][0-9][0-9] | [1-5][0-9][0-9][0-9][0-9] | 6[0-4][0-9][0-9][0-9] | 65[0-4][0-9][0-9] | 655[0-3][0-5])
-      echo
-      echo
-      echo -e "${yellow} 端口 (Port) = ${cyan}${port}${none}"
-      echo "----------------------------------------------------------------"
-      echo
-      break
-      ;;
-    *)
-      error
-      ;;
-    esac
-  done
+	while :; do
+		read -p "$(echo -e "请输入端口 [${magenta}1-65535${none}] Input port (默认Default ${cyan}${default_port}$none):")" port
+		[ -z "$port" ] && port=$default_port
+		case $port in
+		[1-9] | [1-9][0-9] | [1-9][0-9][0-9] | [1-9][0-9][0-9][0-9] | [1-5][0-9][0-9][0-9][0-9] | 6[0-4][0-9][0-9][0-9] | 65[0-4][0-9][0-9] | 655[0-3][0-5])
+			echo
+			echo
+			echo -e "${yellow} 端口 (Port) = ${cyan}${port}${none}"
+			echo "----------------------------------------------------------------"
+			echo
+			break
+			;;
+		*)
+			error
+			;;
+		esac
+	done
 fi
 
 # 域名
 if [[ -z $domain ]]; then
-    echo
-    echo -e "请输入自签证书使用的 ${magenta}域名${none} Input certificate domain"
-    read -p "(默认: learn.microsoft.com): " domain
-    [ -z "$domain" ] && domain="learn.microsoft.com"
-    echo
-    echo
-    echo -e "${yellow} 证书域名 Certificate Domain = ${cyan}${domain}${none}"
-    echo "----------------------------------------------------------------"
-    echo
+	echo
+	echo -e "请输入自签证书使用的 ${magenta}域名${none} Input certificate domain"
+	read -p "(默认: learn.microsoft.com): " domain
+	[ -z "$domain" ] && domain="learn.microsoft.com"
+	echo
+	echo
+	echo -e "${yellow} 证书域名 Certificate Domain = ${cyan}${domain}${none}"
+	echo "----------------------------------------------------------------"
+	echo
 fi
 
 # 密码
 if [[ -z $pwd ]]; then
-    echo -e "请输入 ${yellow}密码${none}"
-    read -p "$(echo -e "(默认ID: ${cyan}${default_uuid}$none):")" pwd
-    [ -z "$pwd" ] && pwd=${default_uuid}
-    echo
-    echo
-    echo -e "${yellow} 密码 (Password) = ${cyan}${pwd}${none}"
-    echo "----------------------------------------------------------------"
-    echo
+	echo -e "请输入 ${yellow}密码${none}"
+	read -p "$(echo -e "(默认ID: ${cyan}${default_uuid}$none):")" pwd
+	[ -z "$pwd" ] && pwd=${default_uuid}
+	echo
+	echo
+	echo -e "${yellow} 密码 (Password) = ${cyan}${pwd}${none}"
+	echo "----------------------------------------------------------------"
+	echo
 fi
 
 # 生成证书
@@ -218,41 +267,40 @@ echo
 echo -e "${yellow}配置 /etc/hysteria/config.yaml${none}"
 echo "----------------------------------------------------------------"
 cat >/etc/hysteria/config.yaml <<-EOF
-listen: :${port}     # 工作端口
+	listen: :${port}     # 工作端口
 
-tls:
-  cert: ${cert_dir}/${domain}.crt    # 证书路径
-  key: ${cert_dir}/${domain}.key     # 证书路径
+	tls:
+	  cert: ${cert_dir}/${domain}.crt    # 证书路径
+	  key: ${cert_dir}/${domain}.key     # 证书路径
 
-auth:
-  type: password
-  password: ${pwd}    # 密码
+	auth:
+	  type: password
+	  password: ${pwd}    # 密码
 
-ignoreClientBandwidth: true
+	ignoreClientBandwidth: true
 
-acl:
-  inline:
-    # 如果你想利用 *ray 的分流规则, 那么在hy2自己的分流规则里面设置全部走socks5出去, 将下面一行的注释取消
-    # - s5_outbound(all)
+	acl:
+	  inline:
+	    # 如果你想利用 *ray 的分流规则, 那么在hy2自己的分流规则里面设置全部走socks5出去, 将下面一行的注释取消
+	    # - s5_outbound(all)
 
-outbounds:
-  # 没有分流规则, 默认生效第一个出站 直接出站
-  - name: direct_outbound
-    type: direct
-  # 如果你想利用 *ray 的分流规则, 那么在hy2自己的分流规则里面设置全部走socks5出去
-  - name: s5_outbound
-    type: socks5
-    socks5:
-      addr: 127.0.0.1:1080
+	outbounds:
+	  # 没有分流规则, 默认生效第一个出站 直接出站
+	  - name: direct_outbound
+	    type: direct
+	  # 如果你想利用 *ray 的分流规则, 那么在hy2自己的分流规则里面设置全部走socks5出去
+	  - name: s5_outbound
+	    type: socks5
+	    socks5:
+	      addr: 127.0.0.1:1080
 
 EOF
-
 
 # 优化 Hy2
 # 将发送、接收两个缓冲区都设置为 16 MB
 cat >/etc/sysctl.d/hysteria.conf <<-EOF
-net.core.rmem_max=16777216
-net.core.wmem_max=16777216
+	net.core.rmem_max=16777216
+	net.core.wmem_max=16777216
 EOF
 
 sysctl -p /etc/sysctl.d/hysteria.conf
@@ -260,9 +308,9 @@ sysctl -p /etc/sysctl.d/hysteria.conf
 # 进程优先级
 mkdir -p /etc/systemd/system/hysteria-server.service.d/
 cat >/etc/systemd/system/hysteria-server.service.d/priority.conf <<-EOF
-[Service]
-CPUSchedulingPolicy=rr
-CPUSchedulingPriority=99
+	[Service]
+	CPUSchedulingPolicy=rr
+	CPUSchedulingPriority=99
 EOF
 
 # 重启 Hy2
@@ -286,7 +334,7 @@ echo
 
 # 如果是 IPv6 那么在生成节点分享链接时, 要用[]把IP包起来
 if [[ $netstack == "6" ]]; then
-    ip="[${ip}]"
+	ip="[${ip}]"
 fi
 echo "---------- 链接 URL ----------"
 hy2_url="hysteria2://${pwd}@${ip}:${port}?alpn=h3&insecure=1&pinSHA256=${pinsha256_cert}#HY2_${ip}"
@@ -301,7 +349,8 @@ echo "---------- END -------------"
 echo "以上节点信息保存在 ~/_hy2_url_ 中"
 
 # 节点信息保存到文件中
-echo $hy2_url > ~/_hy2_url_
-echo "以下两个二维码完全一样的内容" >> ~/_hy2_url_
-qrencode -t UTF8 $hy2_url >> ~/_hy2_url_
-qrencode -t ANSI $hy2_url >> ~/_hy2_url_
+echo $hy2_url >~/_hy2_url_
+echo "以下两个二维码完全一样的内容" >>~/_hy2_url_
+qrencode -t UTF8 $hy2_url >>~/_hy2_url_
+qrencode -t ANSI $hy2_url >>~/_hy2_url_
+tgsbshow
